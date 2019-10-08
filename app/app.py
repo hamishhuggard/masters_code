451b844150fdcaa55e1abdac4c57a3b666d09c9f
Hamish Huggard <hamishh@Hamishs-MacBook-Pro.orion.internal>
2019-10-08 13:57:35 +1300

    removed and renamed app files

--- app/app.py
+++ app/app.py
@@ -1,54 +1,161 @@
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+
+from dash.dependencies import Input, Output, State
 import numpy as np
+import pandas as pd
 import json
 import os
+from datetime import datetime
 
-data_streams_path = os.path.abspath('./fake_data/data_streams.json')
+def load_scenario(i):
+    path = os.path.abspath(f'../Fake data/scenario{i}.csv')
+    data = pd.read_csv(path)
+    data['date'] = data['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%S:%f'))
+    return data
 
-with open(data_streams_path) as f:
-    data_streams = json.loads(f.read())
+def get_tabs(data):
+    return [
+            dcc.Tab(label='Error Rate', children=[
+                html.Div(style={'backgroundColor': colors['background']}, children=[
+                    dcc.Graph(
+                        id='Graph1',
+                        figure={
+                            'data': [
+                                {'x': data['date'], 'y': data[col], 'name': col}
+                                for col in ['warning_level', 'drift_level', 'error_rate']
+                            ],
+                            'layout': {
+                                'plot_bgcolor': colors['background'],
+                                'paper_bgcolor': colors['background'],
+                                'font': {
+                                    'color': colors['text']
+                                }
+                            }
+                        }
+                    )
+                ])
+            ]),
+            dcc.Tab(label='Labels', children=[
+                html.Div(style={'backgroundColor': colors['background']}, children=[
+                    dcc.Checklist(
+                        id = 'label display',
+                        options=[
+                            {'label': 'Predicted labels', 'value': 'predicted'},
+                            {'label': 'True Labels', 'value': 'true'}
+                        ],
+                        value=['true']
+                    ),
+                    dcc.Graph(
+                        id='Graph2',
+                        figure={
+                            'data': [
+                                {'x': data['date'], 'y': data[f'label={i}'], 'name': i}
+                                for i in [1,2,3,4,5]
+                            ],
+                            'layout': {
+                                'plot_bgcolor': colors['background'],
+                                'paper_bgcolor': colors['background'],
+                                'font': {
+                                    'color': colors['text']
+                                }
+                            }
+                        }
+                    )
+                ])
+            ]),
+            dcc.Tab(label='Features', children=[
+                html.Div(style={'backgroundColor': colors['background']}, children=[
+                    dcc.Graph(
+                        id='Graph3',
+                        figure={
+                            'data': [
+                                {'x': data['date'], 'y': data[col], 'name': col}
+                                for col in data.columns if col != 'date' and \
+                                not col.startswith('label=') and col not in ['warning_level', 'drift_level', 'error_rate']
+                            ],
+                            'layout': {
+                                'plot_bgcolor': colors['background'],
+                                'paper_bgcolor': colors['background'],
+                                'font': {
+                                    'color': colors['text']
+                                }
+                            }
+                        }
+                    )
+                ])
+            ]),
+            dcc.Tab(label='Region Drift', children=[
+                html.Div([
+                    html.H1("Region 3 is drifting."),
+                ])
+            ]),
+        ]
 
-x = list(range(len(data_streams['accuracy'])))
+data = load_scenario(1)
 
 app = dash.Dash()
+
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+
+
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': 'white',
+    'text': 'black'
 }
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='GP Referrals Triage Drift Detection',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
+
+app.layout = html.Div([
+    html.H1('GP Referrals Triage Drift Detection', style={
+            'textAlign': 'center', 'margin': '48px 0', 'fontFamily': 'system-ui'}),
+    # html.Label('Scenario',
+    #             style={'fontFamily': 'system-ui', 'maxWidth': '1000px', 'margin': '10px auto'},
+    # ),
+    dcc.Dropdown(
+        id = 'scenario-selector',
+        options=[
+            {'label': 'Real Drift', 'value': 1},
+            {'label': 'Virtual Drift', 'value': 2}
+        ],
+        value=1,
+        style={'fontFamily': 'system-ui', 'maxWidth': '1000px', 'margin': '10px auto'},
+        # content_style={
+        #     'borderLeft': '1px solid #d6d6d6',
+        #     'borderRight': '1px solid #d6d6d6',
+        #     'borderBottom': '1px solid #d6d6d6',
+        #     'padding': '44px'
+        # },
+
     ),
-    html.Div(children='Below is a time series of the error rate of the model and the thresholds.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-    dcc.Graph(
-        id='Graph1',
-        figure={
-            'data': [
-                {'x': stream[0], 'y': stream[1], 'name': name}
-                for name, stream in data_streams.items()
-#                {'x': x, 'y': warnings, 'name': 'Warning Threshold'}, # 'type': 'bar',##
-#                {'x': x, 'y': drifts, 'name': 'Drift Threshold'},
-#                {'x': x, 'y': errs, 'name': u'Error rate'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
+    dcc.Tabs(id="tabs", children=get_tabs(data),
+        style={
+            'fontFamily': 'system-ui'
+        },
+        content_style={
+            'borderLeft': '1px solid #d6d6d6',
+            'borderRight': '1px solid #d6d6d6',
+            'borderBottom': '1px solid #d6d6d6',
+            'padding': '44px'
+        },
+        parent_style={
+            'maxWidth': '1000px',
+            'margin': '0 auto'
         }
     )
 ])
+
+@app.callback(
+    Output(component_id='tabs', component_property='children'),
+    [Input(component_id='scenario-selector', component_property='value')]
+)
+def update_output_div(input_value):
+    global data
+    data = load_scenario(input_value)
+    return get_tabs(data)
+    # return 'You\'ve entered "{}"'.format(input_value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
