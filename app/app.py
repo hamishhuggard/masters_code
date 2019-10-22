e4ff42c06d05f0c047b967bfcc686523e7301cd0
Hamish Huggard <hamishh@wifi-172-23-61-39.uoa-wifi.auckland.ac.nz>
2019-10-23 11:29:58 +1300

    something

--- app/app.py
+++ app/app.py
@@ -16,6 +16,43 @@ def load_scenario(i):
     data = pd.read_csv(path)
     data['date'] = data['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%S:%f'))
     return data
+'''
+The authors describe politics in terms of three dimensions: essentials, influentials, and interchangeables. Essentials are people the political leader needs to keep loyal, influentials are people who the leader sometimes needs to keep loyal, and interchangeables are people who the leader doesn't need to keep loyal on an individual basis. This framework seems kinda strange to me. These seem more like quantitative distinctions than qualititative ones, so I think it would be more natural to put everyone on a single axis of "influence" or "value to the leader". This way there would be an easy way to quantify where a given society falls on the democracy-autocracy spectrum, by taking either the entropy, or the power law index of the "influence" distribution. I don't see an easy way to do this with the essentials/influentials/interchangeables framework (is lots of interchangeables but few influentials better than vice versa?)
+On foreign aid:
+  * State foreign aid seems to be motivated by the interests of the populous rather than humanitarian interests. The most eggregious example of this was the US personally enriching Liberia's dictator Samuel Doe in exchange for his loyalty against communism.
+  * Something I've found confusing is that countries rich in natural resources often do worse in terms of development than those poor in natural resources. It's easy to see why they wouldn't do any better: a dictator simply steals all the value of the natural resources and so none goes into development. But why would these countries actually do <i>worse</i> in development? There are two reasons: if the dictator has more money readily available then they can more successfully supress any political dissidents. And, more interestingly, if there are no natural resources to plunder, the only way for the dictator to enrich themself and pay off their cronies is by taxing a productive population. Thus economic development is in the dictator's interest.
+  * The best way to help a poor country under autocratic rule is 1) don't buy natural resources from it as this only empowers the dictator (see above), 2) reward the dictator for taking steps towards democratization (such as offering safe and luxurious retirements to dictators who agree to step down), 3) don't give the rewards directly, instead place them in escrow accounts which will be opened after certain stipulations are met.
+  * Humanitarian efforts are typically most successful in areas which the local dictator would support anyway. Health, nutrition, and low-level education are all capabilities which make the population more productive, and therefore more wealth-extractable.
+  * Bringing aid or forgiving debts of corrupt and poor countries is often counterproductive, as this only gives the dictator more opportunities to pay off his cronies and entrench his position.
+War:
+  * In democracies, the value of citizen lives is higher, and if the security of the state is at risk, then
+  * Why is Israel so milataristically successful? Partly
+That democrats are more
+A consequence of gerrymandering is that everyone is dissatisfied with the government as a whole, but satisfied with their representitive in particular.
+
+
+Tokenization from scratch: some thoughts
+ * train a character level language model. then iteratively join tokens such that the entropy of the predictions stays around constant
+
+
+I wonder how predictive accuracy scales with the level of abstraction that someone is reasoning at.
+'''
+# LDD Region Drift Algorithm
+def distance(a, b):
+    return len(np.where(a != b))
+
+def knn(centre, population, k):
+    # Return the indices of the k-nearest neighbours of center in population
+    distances = [ distance(centre, x) for x in population ]
+    ret = []
+    for dist in range(len(centre)):
+        for i in np.where(distances == dist):
+            ret.append(i)
+            if len(ret) >= k:
+                return ret
+    #
+    return ret
+
 
 def get_error_tab(data):
     return dcc.Tab(
